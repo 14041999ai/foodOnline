c167c59df3919f473316d1140def411ac4d876d7
14041999ai <as3755081@gmail.com>
2025-01-16 23:42:31 +0530

    user registration form 1

---
+++ accounts/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from accounts.models import User
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
