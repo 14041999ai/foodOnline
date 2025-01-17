ca9685304a0abe19e2c771079382133ac48a79c7
14041999ai <as3755081@gmail.com>
2025-01-17 23:45:58 +0530

    hash password

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
 from accounts.models import User
+from django import forms
 
 class UserForm(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email']
+        fields = ['first_name', 'last_name', 'username', 'email', 'phone_manager', 'role']
