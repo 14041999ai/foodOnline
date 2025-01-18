1003cbeacfbee9f3ddbef8d4c55fd8ccc9761c2d
14041999ai <as3755081@gmail.com>
2025-01-19 01:19:30 +0530

    django form field and non_field errors

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm
 from accounts.models import User
 from django import forms
+from django.core.exceptions import ValidationError
+
 
 class UserForm(ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -9,3 +11,13 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'phone_manager', 'role']
+
+    def clean(self):
+
+        cleaned_data = super().clean()
+        password = cleaned_data["password"]
+        confirm_password = cleaned_data["confirm_password"]
+        if  password != confirm_password:
+            raise forms.ValidationError(
+                    "Password does not match!"
+                )
--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,6 @@ def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
             user = form.save(commit=False)
             password = form.cleaned_data["password"]
             user.role = User.CUSTOMER
@@ -18,17 +17,7 @@ def registerUser(request):
             return  redirect('registerUser')
 
         else:
-            print(f"field.errors {form.errors}")
             print(f"non_field error {form.non_field_errors}")
-            # first_name = form.cleaned_data["first_name"]
-            # last_name = form.cleaned_data["last_name"]
-            # username = form.cleaned_data["username"]
-            # email = form.cleaned_data["email"]
-            # password = form.cleaned_data["password"]
-            # user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
-            # user.role = User.CUSTOMER
-            # user.save()
-            # return  redirect('registerUser')
     else:
         form = UserForm()
 
