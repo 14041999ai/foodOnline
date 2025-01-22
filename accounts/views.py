cb55daf586961808a632cd443c8751211ac45cdf
14041999ai <as3755081@gmail.com>
2025-01-22 21:19:50 +0530

    vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from .forms import UserForm
+from accounts.models import UserProfile
 from vendor.forms import VendorForm
 from .models import User
 
@@ -29,12 +30,30 @@ def registerUser(request):
 
 def registerVendor(request):
 
-    form = UserForm()
-    v_form = VendorForm()
+    if request.method == 'POST':
+
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data["password"]
+            user.role = User.VENDOR
+            user.set_password(password)
+            user.save()
+
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            vendor.user_profile = UserProfile.objects.get(user=user)
+            vendor.save()
+            messages.add_message(request, messages.SUCCESS, "Your account has been created successfully. Please wait for approval.")
+            return redirect('registerVendor')
+    else:
+        form = UserForm()
+        v_form = VendorForm()
 
     context = {
         'form': form,
         'v_form': v_form
     }
-    
+
     return render(request, 'accounts/registerVendor.html', context)
