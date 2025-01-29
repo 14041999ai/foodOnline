6d47f15605c42553914d1bce14560c3067502aaa
14041999ai <as3755081@gmail.com>
2025-01-29 23:45:19 +0530

    admin approval email

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User, UserProfile
+from accounts.utils import send_notification_mail
 
 class Vendor(models.Model):
 
@@ -13,3 +14,23 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.vendor_name
+
+    def save(self, *args, **kwargs):
+        # check if update
+        if self.id is not None:
+            orig = Vendor.objects.get(id=self.id)
+            if orig.is_approved != self.is_approved:
+                mail_template = 'accounts/emails/admin_approval_email.html'
+                context = {
+                    'user': self.user,
+                    'is_approved': self.is_approved
+                }
+                if self.is_approved == True:
+                    #send success mail
+                    mail_subject = 'Congratulations! Your restaurent has been approved.'
+                    send_notification_mail(mail_subject, mail_template, context)
+                else:
+                    # send fail mail
+                    mail_subject = "We're sorry! You are not eligible for publishing your food menu on our marketplace."
+                    send_notification_mail(mail_subject, mail_template, context)
+        return super(Vendor, self).save(*args, **kwargs)
