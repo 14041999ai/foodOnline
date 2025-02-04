11eac812a514a5b509c11cba33e33d71d65932d4
14041999ai <as3755081@gmail.com>
2025-02-04 23:36:08 +0530

    vendor profile form and validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,7 +15,6 @@ class UserForm(ModelForm):
         fields = ['first_name', 'last_name', 'username', 'email', 'phone_manager', 'role']
 
     def clean(self):
-
         cleaned_data = super().clean()
         password = cleaned_data["password"]
         confirm_password = cleaned_data["confirm_password"]
@@ -27,9 +26,25 @@ class UserForm(ModelForm):
 
 class UserProfileForm(ModelForm):
 
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
     profile_picture = forms.FileField(validators=[allow_only_image_validators])
     cover_photo = forms.FileField(validators=[allow_only_image_validators])
 
     class Meta:
         model = UserProfile
-        fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = True
+
+    def clean(self):
+        cleaned_data = super().clean()
+        instance = getattr(self, 'instance', None)
+        if instance and instance.id:
+            cleaned_data['latitude'] = instance.latitude
+            cleaned_data['longitude'] = instance.longitude
+
+        return cleaned_data
