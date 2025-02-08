025e0a7bc4f29eae503e2733263078506f2599ba
14041999ai <as3755081@gmail.com>
2025-02-09 00:33:03 +0530

    add test food and setup admin table

--- menu/models.py
+++ menu/models.py
@@ -3,8 +3,8 @@ from vendor.models import Vendor
 
 class Category(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    category_name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    category_name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100)
     description = models.TextField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
