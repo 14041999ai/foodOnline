d638fd0750555d355ebb04b3205b18e51144d6bd
14041999ai <as3755081@gmail.com>
2025-02-13 20:14:39 +0530

    edit category api

--- vendor/urls.py
+++ vendor/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('menu-builder/', views.menu_builder, name="menu_builder"),
     path('menu-builder/category/<int:pk>', views.food_items_by_category, name='food_items_by_category'),
     path('menu-builder/category/add', views.add_category, name='add_category'),
+    path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
 ]
 
--- vendor/views.py
+++ vendor/views.py
@@ -48,7 +48,7 @@ def vprofile(request):
 def menu_builder(request):
 
     vendor = get_vendor(request)
-    categories = Category.objects.filter(vendor=vendor)
+    categories = Category.objects.filter(vendor=vendor).order_by('created_at')
     context = {
         'categories': categories
     }
@@ -87,3 +87,26 @@ def add_category(request):
         'form': form,
     }
     return render(request, 'vendor/add_category.html', context)
+
+
+def edit_category(request, pk=None):
+
+    category = Category.objects.get(id=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, category)
+        if form.is_valid():
+            category_name = form.cleaned_data['category_name']
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request)
+            category.slug = slugify(category_name)
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Category updated successfully!')
+            return redirect('menu_builder')
+    else:
+        form = CategoryForm(instance=category)
+
+    context = {
+        'form': form,
+        'cat': category
+    }
+    return render(request, 'vendor/edit_category.html', context)
