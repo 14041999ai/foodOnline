77708d61be70248e9bc942fd01571dfc55d01e9c
14041999ai <as3755081@gmail.com>
2025-02-23 22:40:43 +0530

    display fooditems by category

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from vendor.models import Vendor
+from menu.models import Category, FoodItem
+from django.db.models import Prefetch
+
 
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -12,7 +15,12 @@ def marketplace(request):
 
 def vendor_detail(request, vendor_slug):
     vendor = Vendor.objects.get(slug=vendor_slug)
+    available_fooditems = FoodItem.objects.filter(is_available=True)
+    prefetch = Prefetch("fooditems", queryset=available_fooditems)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(prefetch)
+
     context = {
-        "vendor": vendor
+        "vendor": vendor,
+        "categories": categories
     }
     return render(request, 'marketplace/vendor_detail.html', context)
