f5ed7d9fec80f7a1aa37e8dce89bb0cb5832fa2c
14041999ai <as3755081@gmail.com>
2025-03-04 09:00:15 +0530

    decrease cart feature edited

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -80,10 +80,12 @@ $(document).ready(function(e){
             url: url,
             data: data,
             success: function(response){
-
-                console.log(response.cart_counter["cart_count"])
-                $('#cart-counter').html(response.cart_counter["cart_count"]);
-                $('#qty-'+food_id).html(response.qty)
+                if (response.status == 'Failed'){
+                    console.log(response)
+                }else{
+                    $('#cart-counter').html(response.cart_counter["cart_count"]);
+                    $('#qty-'+food_id).html(response.qty)
+                }
             }
         })
     })
@@ -95,4 +97,39 @@ $(document).ready(function(e){
         // console.log(qty)
         $('#'+the_id).html(qty)
     })
+
+    $('.decrease_cart').on('click', function(e){
+        e.preventDefault();
+        
+        food_id = $(this).attr('data-id')
+        url = $(this).attr('data-url')
+        data = {
+            'food_id':food_id,
+        }
+
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: data,
+            success: function(response){
+
+                console.log(response)
+                if (response.status == 'Failed'){
+                    console.log(response)
+                }else{
+                    $('#cart-counter').html(response.cart_counter["cart_count"]);
+                    $('#qty-'+food_id).html(response.qty)
+                }
+            }
+        })
+    })
+
+    // place the cart item quantity on load
+    $('.item_qty').each(function(){
+        var the_id = $(this).attr('id')
+        var qty = $(this).attr('data-qty')
+        // console.log(qty)
+        $('#'+the_id).html(qty)
+    })
+    
 });
