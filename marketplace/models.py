60fa6f62fa67c71e86bf282fd9d9063d4b91cfb8
14041999ai <as3755081@gmail.com>
2025-04-17 21:04:49 +0530

    tax model

--- marketplace/models.py
+++ marketplace/models.py
@@ -11,3 +11,21 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"Cart ({self.user.username})"
+
+class Tax(models.Model):
+    tax_type = models.CharField(max_length=20, unique=True)
+    tax_percentage = models.DecimalField(decimal_places=2, max_digits=4, verbose_name='Tax Percentage (%)')
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'tax'
+    
+        def __str__(self):
+            return 
+    
+        def __unicode__(self):
+            return 
+    
+
+    def __str__(self):
+        return self.tax_type
